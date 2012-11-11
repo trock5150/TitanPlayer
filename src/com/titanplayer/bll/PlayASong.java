c55bbaeeab22f09fdb7da5cafd089fd791537801
Terry Rockfield <trock5150@embarqmail.com>
2012-11-10 20:45:29 -0500

    New class to play a song in a new thread

---
+++ src/com/titanplayer/bll/PlayASong.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.titanplayer.bll;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Rocky
+ */
+public class PlayASong extends Thread{
+    
+    public void run(){
+        System.out.println("Playing the song in a new thread");
+        try {
+            //built in 2 minute sleep period to simulate a short song
+            Thread.sleep(200000);
+        } catch (InterruptedException ex) {
+            System.out.println("Skipping to next song!");
+        }
+        
+    }
+    
+    
+}
