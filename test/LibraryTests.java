f7abe2a75ee874cfe7d568a6202ad29e8bfb1021
Terry Rockfield <trock5150@embarqmail.com>
2012-11-10 20:46:19 -0500

    New test for skipping a song.

--- test/LibraryTests.java
+++ test/LibraryTests.java
@@ -4,14 +4,13 @@
  */
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import org.junit.*;
 import com.titanplayer.bll.Library;
 import com.titanplayer.bll.PlayList;
 import com.titanplayer.bll.Song;
+import com.titanplayer.bll.PlayASong;
 
 /**
  *
@@ -149,7 +148,7 @@ public class LibraryTests {
         int searchIndex = 0;
         String songInfo = null;
         
-        System.out.println("Playlist \"" + myPlaylist.getPlaylistName() + "\" consists of the following songs:\n" );
+        System.out.println("Playlist Test 1: Playlist \"" + myPlaylist.getPlaylistName() + "\" consists of the following songs:\n" );
         for(int i = 0;i < myPlaylist.getPlaylistSize();i++){
             searchIndex = myPlaylist.returnPlaylistEntry(i);
             songInfo = myLibrary.displayPlayListSong(searchIndex);
@@ -157,4 +156,71 @@ public class LibraryTests {
         }
             
     }
+    
+    @Test
+    public void skipASongTest() throws InterruptedException{
+        //Build test library
+        Library myLibrary = new Library();
+        
+        myLibrary.addNewSong(new Song("Roll With The Changes", "REO Speedwagon","You Can Tune A Piano But You Can't Tune A Fish"
+                ,"c:\\mymusic\\REO\\Tuna\\RollWithTheChanges.mp3"));
+        myLibrary.addNewSong(new Song("Nothing But A Good Time", "Poison","Open Up And Say AHHH","c:\\mymusic\\Poison\\OpenUp\\NothingButAGoodTime.mp3"));
+        myLibrary.addNewSong(new Song("Crystal Ball", "Styx","Crystal Ball","c:\\mymusic\\Styx\\CrystalBall\\CrystalBall.mp3"));
+        myLibrary.addNewSong(new Song("Crystal Ball", "Styx","Caught In The Act","c:\\mymusic\\Styx\\CaughtInTheAct\\CrystalBall.mp3"));
+        myLibrary.addNewSong(null);
+        
+        //build test playlist
+        PlayList myPlaylist = new PlayList();
+        myPlaylist.setNewName("My Playlist");
+        
+        List<Song> selectedSongs = new ArrayList<Song>();
+        
+        selectedSongs.add(new Song("Roll With The Changes", "REO Speedwagon","You Can Tune A Piano But You Can't Tune A Fish"
+                ,"c:\\mymusic\\REO\\Tuna\\RollWithTheChanges.mp3"));
+        selectedSongs.add(new Song("Crystal Ball", "Styx","Caught In The Act","c:\\mymusic\\Styx\\CaughtInTheAct\\CrystalBall.mp3"));
+        selectedSongs.add(new Song("Crystal Ball", "Styx","Crystal Ball","c:\\mymusic\\Styx\\CrystalBall\\CrystalBall.mp3"));
+        
+        //Print out playlist for reference
+        for(Song eachSong : selectedSongs){
+            int libraryLocation = myLibrary.getSongLocation(eachSong);
+            System.out.println("libraryLocation = " + libraryLocation);
+            myPlaylist.setLibraryLocation(libraryLocation);
+        }
+
+        assertEquals(3,myPlaylist.getPlaylistSize());
+        
+        int searchIndex = 0;
+        String songInfo = null;
+        
+        System.out.println("Playlist Test 2: Playlist \"" + myPlaylist.getPlaylistName() + "\" consists of the following songs:\n" );
+        for(int i = 0;i < myPlaylist.getPlaylistSize();i++){
+            searchIndex = myPlaylist.returnPlaylistEntry(i);
+            songInfo = myLibrary.displayPlayListSong(searchIndex);
+            System.out.print(songInfo + "\n");
+        }
+        
+        for(Song currentSong: selectedSongs){
+            
+            //Song must play in a new thread in order to allow main thread to interrupt it
+            Thread t = new Thread(new PlayASong());
+            t.start();
+            
+            int i=0;
+            
+            //this will simulate the 'Next Song button' being pressed to skip to the next song
+            while(t.isAlive()){
+                if (i > 0){
+                    System.out.println(i*10 + " Seconds into playing song: "+currentSong.showPath(currentSong));
+                }
+                
+                t.join(10000);
+                i++;
+                
+                if(i == 5 && t.isAlive()){
+                    t.interrupt();
+                }
+            }
+        }
+    }
+        
 }
