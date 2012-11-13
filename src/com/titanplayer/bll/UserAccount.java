62b02128bb0846e3f8660bd51188e282157966c0
Terry Rockfield <trock5150@embarqmail.com>
2012-11-12 20:24:39 -0500

    Changes made 11/12/12.

---
+++ src/com/titanplayer/bll/UserAccount.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.titanplayer.bll;
+
+/**
+ *
+ * @author Rocky
+ */
+public class UserAccount {
+    private String userName;
+    private String userEmail;
+    private String password;
+    
+    public UserAccount(String userName, String userEmail, String password) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.password = password;
+    }
+    
+    
+}
