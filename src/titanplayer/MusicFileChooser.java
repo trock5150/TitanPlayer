b3e8f7117d6804ec6f0eff8e525c1fe32e69c360
Terry Rockfield <trock5150@embarqmail.com>
2012-11-16 19:57:32 -0500

    Changes 11/16/2012

---
+++ src/titanplayer/MusicFileChooser.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package titanplayer;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.*;
+
+/**
+ *
+ * @author Rocky
+ */
+public class MusicFileChooser extends JFrame {
+
+    public MusicFileChooser() {
+	
+        JFileChooser fc = new JFileChooser();
+  
+        int returnVal = fc.showOpenDialog(this);
+        
+        File selectedFile = null;
+        
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            selectedFile = fc.getSelectedFile();
+        }  
+    }
+}
